7dda1aeb8f5feb66d3d8e5ffadb03ff2a5899085
Ryan Flaherty <rcflaherty@gmail.com>
2016-04-11 12:47:11 -0700

    form val added, edit reverts on cancel

--- ryan-flaherty/app/js/controllers/editShowCtrl.js
+++ ryan-flaherty/app/js/controllers/editShowCtrl.js
@@ -4,18 +4,22 @@ module.exports = function(app) {
   app.controller('editShowCtrl', ['$scope', '$location', '$http', 'Auth', function($scope, $location, $http, Auth) {
 
     $scope.edit = false;
+    $scope.updateShow = {};
+    $scope.cancelShow = {};
 
     $scope.getShow = function() {
       var url = $location.path();
       url = url.split('/');
       var id = url[url.length - 1];
       $http.get('/shows/' + id).success(function(response) {
-       $scope.show = response;
-       $scope.show.date = new Date($scope.show.date);
+        $scope.show = response;
+        $scope.show.date = new Date($scope.show.date);
+        $scope.updateShow = $scope.show;
+        $scope.cancelShow = angular.copy($scope.show);
       });
     };
 
-    $scope.putShow = function(show) {
+    $scope.putShow = function(updateShow) {
       $http({
         method: 'PUT',
         url: '/shows/' + $scope.show._id,
@@ -45,8 +49,13 @@ module.exports = function(app) {
       });
     };
 
-    $scope.shit = function() {
-      console.log('Shit');
+    $scope.cancel = function(){
+      $scope.edit = false;
+      $scope.show.cost = $scope.cancelShow.cost;
+      $scope.show.date = $scope.cancelShow.date;
+      $scope.show.bands = $scope.cancelShow.bands;
+      $scope.show.regBands = $scope.cancelShow.regBands;
+      $scope.show.venue = $scope.cancelShow.venue;
     };
 
   }]);
